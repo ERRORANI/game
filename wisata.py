7f7aa0b8aef144b721be918d1e2d87a94a267581
ERRORANI <dedezakybontot@gmail.com>
2024-12-16 23:10:58 +0800

    Create wisata.py

---
+++ wisata.py
@@ -0,0 +1,9 @@
+wisata = {
+    "Raja Ampat": "Destinasi diving terkenal di Papua dengan pemandangan bawah laut yang indah.",
+    "Borobudur": "Candi Buddha terbesar di dunia yang ada di Jawa Tengah.",
+    "Bromo": "Gunung berapi aktif yang menjadi ikon Jawa Timur.",
+    "Tana Toraja": "Daerah di Sulawesi Selatan yang terkenal dengan tradisi pemakaman unik.",
+    "Labuan Bajo": "Kota di Nusa Tenggara Timur, gerbang menuju Pulau Komodo.",
+    "Danau Toba": "Danau vulkanik terbesar di dunia yang berada di Sumatera Utara.",
+    "Pulau Derawan": "Pulau di Kalimantan Timur yang terkenal dengan keindahan bawah lautnya.",
+    "Kepulauan Seribu": "G
